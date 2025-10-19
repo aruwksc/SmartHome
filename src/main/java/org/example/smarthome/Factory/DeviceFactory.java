b7eb9779321a34f1575fea43a7fddf7589726dd0
aruwksc <240966@astanait.edu.kz>
2025-10-19 17:18:52 +0500

    Created DeviceFactory class to instantiate concrete device objects

---
+++ src/main/java/org/example/smarthome/Factory/DeviceFactory.java
@@ -0,0 +1,26 @@
+package org.example.smarthome.Factory;
+
+import org.example.smarthome.device.*;
+
+public class DeviceFactory {
+    public enum DeviceType {
+        LIGHT,
+        MUSIC_SYSTEM,
+        THERMOSTAT,
+        SECURITY_CAMERA
+    }
+    public static Device createDevice(DeviceType deviceType) {
+        switch (deviceType) {
+            case LIGHT:
+                return new Light();
+            case MUSIC_SYSTEM:
+                return new MusicSystem();
+             case THERMOSTAT:
+                 return new Thermostat();
+             case SECURITY_CAMERA:
+                  return new SecurityCamera();
+              default:
+                   throw new IllegalArgumentException("Unknown device type");
+        }
+    }
+}
